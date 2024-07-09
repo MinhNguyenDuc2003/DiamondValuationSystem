b9e13b4ace824e465d68c5d2fa8940a613951cb1
JustinDo2909 <hieudeptraivodichh@gmail.com>
2024-07-09 10:07:58 +0700

    be

--- BackEnd/DiamondValuationSystem/SystemParent/SystemAdmin/src/main/java/com/diamondvaluation/admin/repository/rapaport/DiamondAttributeRepository.java
+++ BackEnd/DiamondValuationSystem/SystemParent/SystemAdmin/src/main/java/com/diamondvaluation/admin/repository/rapaport/DiamondAttributeRepository.java
@@ -12,6 +12,6 @@ import com.diamondvaluation.common.diamond_information.DiamondAttribute;
 public interface DiamondAttributeRepository extends JpaRepository<DiamondAttribute, Integer> {
 	List<DiamondAttribute> findAllByCaratId(Integer caratId);
 	
-	@Query(value = "SELECT * FROM diamond_attributes a WHERE a.carat_id= :carat_id AND a.color= :color AND a.clarity= :clarity", nativeQuery = true)
+	@Query(value = "SELECT * FROM diamond_attributes a WHERE a.carat_id = :carat_id AND a.color= :color AND a.clarity= :clarity", nativeQuery = true)
 	Optional<DiamondAttribute> findNumberBy4C(@Param("carat_id")Integer caratId,@Param("color") String color,@Param("clarity") String clarity);
 }
